78518fcb8c89a62bd41cb8d7baefbb2e45c3e300
Julio Ramos <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-10-19 18:08:19 +0200

    v2

---
+++ bd/configuracion.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+      Conexión con la base de datos (empleados, MySQL)
+      Autor: Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
+      Año 2021
+    */
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PW", "");
+    define("DB", "empresa");
+?>
